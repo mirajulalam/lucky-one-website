57aab0780c4cb4a1c32df6046d217c84cee794fa
mirajul alam <alifmirajulalam@gmail.comm>
2022-03-25 21:18:48 +0600

    card components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Card from './Components/Card/Card';
 
 function App() {
   return (
     <div>
       <h1 className='header-title'>Choese your favorite food</h1>
+      <Card></Card>
     </div>
   );
 }
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Card.css'
+const Card = () => {
+    return (
+        <div>
+            <h1>This is card</h1>
+        </div>
+    );
+};
+
+export default Card;
