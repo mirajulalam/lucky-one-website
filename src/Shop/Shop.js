537290797081a06a67d6774594702e5afde3ce2f
mirajul alam <alifmirajulalam@gmail.comm>
2022-03-26 23:07:51 +0600

    product item disolay show done

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -6,20 +6,19 @@ const Shop = (props) => {
     const { name, picture, price } = props.product;
 
     return (
-        <div>
-            <div className="product">
+
+        <div id="product">
+            <div className='product-container'>
                 <div className="product-info">
                     <img src={picture} alt=""></img>
                     <h3>Name: {name}</h3>
                     <h4>Price: ${price}</h4>
                 </div>
-                <div>
-                    <button onClick={() => handleAddtoCart(product)} className='btn-cart'>
-                        AddToCart <BsFillCartFill />
-                    </button>
-                </div>
-            </div>
 
+                <button onClick={() => handleAddtoCart(product)} className='btn-cart'>
+                    AddToCart <BsFillCartFill />
+                </button>
+            </div>
         </div>
 
     );
